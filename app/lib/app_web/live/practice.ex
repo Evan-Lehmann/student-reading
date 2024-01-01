b82146da49be1bf50e231622f388a45113d6cc07
Evan Lehmann <evanlehmann22@gmail.com>
2024-01-01 17:17:14 -0600

    update cash based on mcq result

--- app/lib/app_web/live/practice.ex
+++ app/lib/app_web/live/practice.ex
@@ -1,6 +1,7 @@
 defmodule AppWeb.Practice do
   use AppWeb, :live_view
   alias App.Quiz
+  alias App.Accounts
 
   def mount(_params, _session, socket) do
     form = to_form(%{}, as: "mcq_attempt")
@@ -52,10 +53,12 @@ defmodule AppWeb.Practice do
               <button disabled class="rounded-lg bg-emerald-700 px-2 py-1 text-white">
                 <%= answer.content %>
               </button>
+              <span class="font-bold text-emerald-600">+$25</span>
             <% else %>
               <button disabled class="rounded-lg bg-red-700 px-2 py-1 text-white">
                 <%= answer.content %>
               </button>
+              <span class="font-bold text-red-600">-$25</span>
             <% end %>
             <hr>
           <% else %>
@@ -69,7 +72,7 @@ defmodule AppWeb.Practice do
 
       <.simple_form for={@form} phx-submit="check">
         <%= if @view == "changed"  do %>
-          <.input field={@form[:answer_id]} type="text" value={@selected} readonly required />
+          <.input field={@form[:answer_id]} type="hidden" value={@selected} readonly required />
           <.button phx-disable-with="Changing" class="w-full">
             Submit
           </.button>
@@ -89,11 +92,21 @@ defmodule AppWeb.Practice do
 
   def handle_event("check", %{"mcq_attempt" => %{"answer_id" => answer_id}}, socket) do
     result = Quiz.is_mcq_answer_correct(String.to_integer(answer_id))
+
+    if result == true do
+      Accounts.update_user_cash(socket.assigns.current_user, %{"cash" => Integer.to_string(socket.assigns.current_user.cash + 25)})
+    end
+
+    if result == false do
+      Accounts.update_user_cash(socket.assigns.current_user, %{"cash" => Integer.to_string(socket.assigns.current_user.cash - 15)})
+    end
+
     {:noreply, assign(socket, view: "checked", result: result)}
   end
 
   def handle_event("next", _params, socket) do
     rand = Enum.random(socket.assigns.list_mcqs).id
+
     {:noreply, assign(socket, view: nil, selected: nil, result: nil, curr_mcq: Quiz.get_mcq!(rand), curr_answers: Quiz.get_answers_of_mcq(rand))}
   end
 end
