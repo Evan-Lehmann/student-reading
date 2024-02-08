bc258c089ad20af25c74c3deebb87e40756da5f0
Evan Lehmann <evanlehmann22@gmail.com>
2024-02-07 23:12:39 -0600

    simplify mcq question layout

--- app/lib/app_web/live/play_easy.ex
+++ app/lib/app_web/live/play_easy.ex
@@ -5,15 +5,14 @@ defmodule AppWeb.PlayEasy do
   alias App.Accounts
 
   def mount(_params, _session, socket) do
-    form = to_form(%{}, as: "mcq_attempt")
     curr_points = socket.assigns.current_user.points
     if connected?(socket) do
       mcq_word = Quiz.get_mcq_by_difficulty("easy")
       inc_words = Quiz.get_inc_words(mcq_word)
       all_words = Enum.shuffle([mcq_word | inc_words])
-      {:ok, assign(socket, word: mcq_word, all_words: all_words, view: nil, result: nil, selected: nil, form: form, points: curr_points)}
+      {:ok, assign(socket, word: mcq_word, all_words: all_words, view: nil, result: nil, selected: nil, points: curr_points)}
     else
-      {:ok, assign(socket, word: nil, all_words: nil, view: nil, result: nil, selected: nil, form: form, points: curr_points)}
+      {:ok, assign(socket, word: nil, all_words: nil, view: nil, result: nil, selected: nil, points: curr_points)}
     end
   end
 
@@ -37,8 +36,10 @@ defmodule AppWeb.PlayEasy do
         <div class="position-absolute top-0 end-0">
           <.link href={~p"/play"}><button type="button" class="btn-close m-3" aria-label="Close"></button></.link>
         </div>
-        <h1 class="h1 mt-3 text-center">Vocab</h1>
-        <p class="lead mb-3">Your Points: <span class="bg-points text-points rounded-full px-2 font-medium leading-6"><%= @points %> </span></p>
+
+        <div class="position-absolute top-0 start-0" style={"margin-left:5.5rem;margin-top:16px;"}>
+          <p class="h5">Your Points: <span class="bg-points text-points rounded-full px-2 font-medium leading-6"><%= @points %> </span></p>
+        </div>
 
         <script>
           function playSound(word){
@@ -47,7 +48,7 @@ defmodule AppWeb.PlayEasy do
           }
         </script>
 
-        <button :if={is_nil(@word) == false} onclick={"playSound('#{@word}')"} class="btn btn-outline-dark d-flex align-items-center justify-content-center mt-2 mb-3">
+        <button :if={is_nil(@word) == false} onclick={"playSound('#{@word}')"} class="btn btn-regular d-flex align-items-center justify-content-center mb-2 w-full" style={"max-width:500px;min-width:250px;margin-top:25px;"}>
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-volume-up-fill m-0 p-0 d-inline-block " viewBox="0 0 16 16">
             <path d="M11.536 14.01A8.47 8.47 0 0 0 14.026 8a8.47 8.47 0 0 0-2.49-6.01l-.708.707A7.48 7.48 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303z"></path>
             <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.48 5.48 0 0 1 11.025 8a5.48 5.48 0 0 1-1.61 3.89z"></path>
@@ -56,78 +57,68 @@ defmodule AppWeb.PlayEasy do
           <span class="lead">What word is this?</span>
         </button>
 
-        <%= if is_nil(@word) == false do %>
-          <div class="container mt-3">
-            <div class="row">
+
+
+        <div class="container px-0" style={"max-width:500px"}>
+          <div class="row">
+            <%= if is_nil(@word) == false do %>
               <%= for answer <- @all_words do %>
-                <div class="col-sm">
-                  <%= if @view == nil || @view == "changed" do %>
-                    <%= if answer == @selected do %>
-                      <button disabled class="btn btn-dark w-full">
-                        <b><%= answer %></b>
-                      </button>
+                <%= if @view == nil do %>
+                  <div class="col">
+                    <button class="btn btn-outline-dark w-full" phx-click="check" phx-value-answer={answer}>
+                      <%= answer %>
+                    </button>
+                  </div>
+                <% else %>
+                  <%= if answer == @selected do %>
+                    <%= if @result == true do %>
+                      <div class="col">
+                        <button disabled class="btn btn-success w-full">
+                          <%= answer %>
+                        </button>
+                        <span class="font-bold text-emerald-600">+25</span>
+                      </div>
                     <% else %>
-                      <button class="btn btn-outline-dark w-full" phx-click="select" phx-value-answer={answer}>
-                        <b><%= answer %></b>
-                      </button>
+                      <div class="col">
+                        <button disabled class="btn btn-danger w-full">
+                          <%= answer %>
+                        </button>
+                        <span class="font-bold text-red-600">-25</span>
+                      </div>
                     <% end %>
                   <% else %>
-                    <%= if answer == @selected do %>
-                      <%= if @result == true do %>
-                        <button class="btn btn-success w-full">
-                          <b><%= answer %></b>
-                        </button>
-                        <span class="font-bold text-emerald-600">+$25</span>
-                      <% else %>
-                        <button disabled class="btn btn-danger w-full">
-                          <b><%= answer %></b>
-                        </button>
-                        <span class="font-bold text-red-600">-$25</span>
-                      <% end %>
-                    <% else %>
+                    <div class="col">
                       <button class="btn btn-outline-dark w-full" disabled>
-                        <b><%= answer %></b>
+                        <%= answer %>
                       </button>
-                    <% end %>
+                    </div>
                   <% end %>
-                </div>
+                <% end %>
               <% end %>
-            </div>
-          </div>
-
-          <.simple_form for={@form} phx-submit="check">
-            <%= if @view == "changed"  do %>
-              <.input field={@form[:answer]} type="hidden" value={@selected} readonly required />
-              <.button class="w-full">
-                Submit
-              </.button>
             <% end %>
-          </.simple_form>
+          </div>
+        </div>
 
-          <.button :if={@view == "checked"} phx-click="next">Next</.button>
-        <% end %>
+        <button class="mt-2 lead" :if={@view == "checked"} phx-click="next"><b>Next →</b></button>
+
       </div>
     </main>
     """
   end
 
-  def handle_event("check", %{"mcq_attempt" => %{"answer" => answer}}, socket) do
+  def handle_event("check", %{"answer" => answer}, socket) do
     result = nil
     if answer == socket.assigns.word do
       result = true
       points = socket.assigns.points + 25
       Accounts.update_user_points(socket.assigns.current_user, %{"points" => Integer.to_string(points)})
-      {:noreply, assign(socket, view: "checked", result: result, points: points)}
+      {:noreply, assign(socket, view: "checked", result: result, points: points, selected: answer)}
     else
       result = false
       points = socket.assigns.points - 25
       Accounts.update_user_points(socket.assigns.current_user, %{"points" => Integer.to_string(points)})
-      {:noreply, assign(socket, view: "checked", result: result, points: points)}
+      {:noreply, assign(socket, view: "checked", result: result, points: points, selected: answer)}
     end
-  end
-
-  def handle_event("select", %{"answer" => answer}, socket) do
-    {:noreply, assign(socket, selected: answer, view: "changed")}
   end
 
   def handle_event("next", _params, socket) do
